f1028b7b6ab8f648ad859004fd61a9fcb5c81449
DConstantine9 <constantinedeniskkin@gmail.com>
2020-03-24 18:33:02 +0300

    added styles

--- src/quote.js
+++ src/quote.js
@@ -15,8 +15,9 @@ export default class Quote extends React.Component {
   } 
 
   quoteRandomizer() {
-    let item = this.getRandomInt(0, this.state.quotes.length)
+    let item = this.getRandomInt(0, this.state.quotes.length - 1)
     let rand = this.state.quotes[item]
+
      this.setState({
       quote: rand.quote,
       author: rand.author
@@ -31,7 +32,6 @@ export default class Quote extends React.Component {
     fetch("https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json")
     .then(data => data.json())
     .then(d => this.setState({quotes: d}))
-    
   }
 
   render() {
@@ -39,9 +39,9 @@ export default class Quote extends React.Component {
       <div id="quote-box"> 
         <div id="text">{this.state.quote}</div>
         <div className="buttons">
-          <div id="author">{this.state.author}</div>
+          <div id="author">- {this.state.author}</div>
           <button onClick={this.quoteRandomizer} id="new-quote">new quote</button>
-          <a id="tweet-quote" href="twitter.com/intent/tweet">tweet</a>
+          <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${this.state.quote}`}><i className="fab fa-twitter-square"></i></a>
         </div>
       </div>
     )
